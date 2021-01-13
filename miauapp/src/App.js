204a778fe8c9ed82ccafb66f9077c77ee4ad65b3
Tomi Enberg <johan1954@Tomis-MBP.home>
2021-01-13 13:49:08 +0200

    Files cleaned up and added file structurecd

--- miauapp/src/App.js
+++ miauapp/src/App.js
@@ -1,14 +1,18 @@
-import logo from './logo.svg';
+import React from  'react';
+import {BrowserRouter} from 'react-router-dom';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
+
         <p>
           miau <code>src/App.js</code> and save to reload.
         </p>
+        <button>
+          Paina nappulaa
+        </button>
         <a
           className="App-link"
           href="https://reactjs.org"

7815fd4c5c7d3523efc5b43637d7c3c9cae9ea79
Petteri Mäkelä <p-mak@pop-os.localdomain>
2021-01-14 13:36:38 +0200

    storypage config

--- miauapp/src/reactComponents/pages/StoryPage.js
+++ miauapp/src/reactComponents/pages/StoryPage.js
@@ -1,7 +1,39 @@
 import { React } from 'react';
-
+import TextArea from '../components/TextArea.js';
+import StoryImage from '../components/StoryImage.js';
+import Button from '../components/Button.js';
 
 
 export default function StoryPage(props) {
-    return (<></>);
+
+  let pageinformation = {
+    text: "newStory.. ",
+    image: "storyimage here"
+  }
+
+  const handlePageChange = (event) => {
+      let msg = {tab: event.target.tab}
+      props.handlePageChange(msg);
+  }
+  
+  return (
+    <div>
+      <TextArea
+      text={pageinformation.text}
+      classPosition={""}
+      classStyle={""}
+      />
+      <StoryImage
+      image={pageinformation.image}
+      classPosition={""}
+      classStyle={""}
+      />
+      <Button
+      buttonText={""}
+      classPosition={""}
+      classStyle={""}
+      OnClick={handlePageChange}/>
+
+    </div>
+  );
 }

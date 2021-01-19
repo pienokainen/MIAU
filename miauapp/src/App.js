965636da52ee453194e7aadb1ab7ea2369d2786d
pienokainen <77006105+pienokainen@users.noreply.github.com>
2021-01-19 15:40:06 +0200

    global styles implented

--- miauapp/src/App.js
+++ miauapp/src/App.js
@@ -3,19 +3,22 @@ import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
 import StoryRoute from "./reactComponents/routes/StoryRoute";
 import { UpdateStoryItems } from "./services";
 import PageRenderer from "./reactComponents/pages/index.js";
-import globalStyles from "./styles/globalStyles.css";
+import "./styles/globalStyles.css";
 import LandingPage from "./reactComponents/pages/LandingPage";
+
 
 function App() {
   return (
     <Router>
-      <div className={globalStyles}>
+      <body className={"bgrStyle"} >
+        <div className={"globalStyles"}>
         <Switch>
           <Route exact path="/:storyId">
             <StoryRoute />
           </Route>
         </Switch>
-      </div>
+        </div>
+      </body>
     </Router>
   );
 }

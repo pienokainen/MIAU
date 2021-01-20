a032befe0a660004ac510dbd5a5612f796dad9ff
Tomi Enberg <johan1954@Tomis-MBP.home>
2021-01-20 16:16:35 +0200

    Story page updated

--- miauapp/src/reactComponents/pages/StoryPage.js
+++ miauapp/src/reactComponents/pages/StoryPage.js
@@ -1,36 +1,33 @@
-import { React } from 'react';
-import TextArea from '../components/TextArea.js';
-import StoryImage from '../components/StoryImage.js';
-import Button from '../components/Button.js';
+import { React } from "react";
+import TextArea from "../components/TextArea.js";
+import StoryImage from "../components/StoryImage.js";
+import Button from "../components/Button.js";
 
-
-export default function StoryPage(props) {
-
+export default function StoryPage({ handlePageChange, pageInformation }) {
   const handlePageChange = (event) => {
     props.handlePageChange();
-  }
+  };
 
   return (
-
     <div>
       {/* Story text area */}
       <TextArea
-      text={props.pageInformation.text}
-      classPosition={""}
-      classStyle={""}
+        text={pageInformation.text}
+        classPosition={""}
+        classStyle={""}
       />
       {/* Story image */}
       <StoryImage
-      image={props.pageInformation.storyImage.imageLocation}
-      classPosition={""}
-      classStyle={""}
+        image={pageInformation.storyImage.imageLocation}
+        classPosition={""}
+        classStyle={""}
       />
       {/* Next page button */}
       <Button
-      classPosition={"nextpage-button"} 
-      classStyle={"nextpage-button-style"}
-      onClick={handlePageChange}/>
-
+        classPosition={"nextpage-button"}
+        classStyle={"nextpage-button-style"}
+        onClick={handlePageChange}
+      />
     </div>
   );
 }

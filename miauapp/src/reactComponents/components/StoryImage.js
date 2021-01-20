88caf5707bb56e661e69945c86ad65ae837b0e61
pienokainen <77006105+pienokainen@users.noreply.github.com>
2021-01-20 16:37:23 +0200

    story images added

--- miauapp/src/reactComponents/components/StoryImage.js
+++ miauapp/src/reactComponents/components/StoryImage.js
@@ -4,8 +4,9 @@ import './styles.css';
 
 export default function StoryImage(props) {
 
+    let imageLocation = "./databaseMock/images/suojatie.png";
     return (
         <div class={props.classPosition}>
-
+            <h3> new storyImage </h3>
         </div>);
 }

34577387dda2d9a2eab00bc651889511df23e36f
Tomi Enberg <tomi.enberg@student.lut.fi>
2021-01-21 19:29:51 +0200

    Questionnaire page updated

--- miauapp/src/reactComponents/pages/QuestionnairePage.js
+++ miauapp/src/reactComponents/pages/QuestionnairePage.js
@@ -1,3 +1,4 @@
+import { getDefaultNormalizer, waitFor } from "@testing-library/react";
 import { React, useState } from "react";
 import Button from "../components/Button.js";
 import Points from "../components/Points.js";
@@ -18,18 +19,47 @@ export default function QuestionnairePage({
     },
   });
 
-  const handleClick = (buttonIndex) => {
-    if (buttonIndex === -1) {
-      handlePageChange();
-    }
+  const [showFeedback, setShowFeedback] = useState(false);
+  const [showNextPageButton, setShowNextPageButton] = useState(false);
+  const [feedbackMessage, setFeedbackMessage] = useState("NONE");
 
+  const getFeedbackText = (alternativeIndex) => {
+    switch (alternativeIndex) {
+      case 0:
+        return pageInformation.feedback.alternative1;
+      case 1:
+        return pageInformation.feedback.alternative1;
+      case 2:
+        return pageInformation.feedback.alternative1;
+      case 3:
+        return "Hy-vä, tie-na-sit yh-den ko-li-kon!";
+      case 4:
+        return "Voi ei, nyt me-ni pie-leen, y-ri-tä uu-del-leen";
+      default:
+        return;
+    }
+  };
+
+  const answerSuccess = () => {
+    setTimeout(function () {
+      setShowNextPageButton(true);
+    }, 4000);
+  };
+
+  const updateFeedback = (buttonIndex) => {
+    setFeedbackMessage(getFeedbackText(buttonIndex));
+    setShowFeedback(true);
+  };
+
+  const handleAnswerClick = (buttonIndex) => {
     if (buttonIndex === pageInformation.correct) {
-      // TODO: logic to handle positive feedback.
+      updateFeedback(3);
       setPoints(points + 1);
-      handlePageChange();
+      answerSuccess();
       return;
     }
 
+    updateFeedback(4);
     let newState = isDisabledButton;
     newState[buttonIndex] = true;
     setIsDisabledButton({
@@ -38,6 +68,10 @@ export default function QuestionnairePage({
         [buttonIndex]: true,
       },
     });
+  };
+
+  const handleNewPage = () => {
+    handlePageChange();
   };
 
   return (
@@ -56,45 +90,51 @@ export default function QuestionnairePage({
         <div className="button-group">
           <Button
             isDisabled={isDisabledButton.buttons[0]}
-            bgrUrl={pageInformation.questionImages[0].alternative1}
+            bgrUrl={pageInformation.questionImages.alternative1}
             classPosition={"questionnaire-button"}
             classStyle={"questionnaire-button-style"}
             onClick={() => {
-              handleClick(0, this);
+              handleAnswerClick(0, this);
             }}
           />
           <Button
             isDisabled={isDisabledButton.buttons[1]}
-            bgrUrl={pageInformation.questionImages[0].alternative2}
+            bgrUrl={pageInformation.questionImages.alternative2}
             classPosition={"questionnaire-button"}
             classStyle={"questionnaire-button-style"}
             onClick={() => {
-              handleClick(1, this);
+              handleAnswerClick(1, this);
             }}
           />
           <Button
             isDisabled={isDisabledButton.buttons[2]}
-            bgrUrl={pageInformation.questionImages[0].alternative3}
+            bgrUrl={pageInformation.questionImages.alternative3}
             classPosition={"questionnaire-button"}
             classStyle={"questionnaire-button-style"}
             onClick={() => {
-              handleClick(2, this);
+              handleAnswerClick(2, this);
             }}
           />
         </div>
       </div>
-      <TextArea
-        text={pageInformation.text}
-        classPosition={"questionnaire-text-area"}
-        classStyle={"questionnaire-text-area-style"}
-      />
-      <Button
-        classPosition={"nextpage-button"}
-        classStyle={"nextpage-button-style"}
-        onClick={() => {
-          handleClick(-1);
-        }}
-      />
+      {showFeedback ? (
+        <TextArea
+          text={feedbackMessage}
+          classPosition={"feedback-text-area"}
+          classStyle={"feedback-text-area-style"}
+        />
+      ) : (
+        <div />
+      )}
+      {showNextPageButton ? (
+        <Button
+          classPosition={"nextpage-button"}
+          classStyle={"nextpage-button-style"}
+          onClick={handleNewPage}
+        />
+      ) : (
+        <div />
+      )}
     </div>
   );
 }

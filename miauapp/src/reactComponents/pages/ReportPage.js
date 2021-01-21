35a2c4edf513c202dea99345530aa46d1425de55
Ella-Mari <77006095+Ella-Mari@users.noreply.github.com>
2021-01-21 13:52:54 +0200

    stipendpage style changed

--- miauapp/src/reactComponents/pages/ReportPage.js
+++ miauapp/src/reactComponents/pages/ReportPage.js
@@ -1,5 +1,6 @@
 import { React } from "react";
 import Stipend from "../components/Stipend.js";
+import Button from "../components/Button.js";
 
 export default function ReportPage(props) {
   // const handlePageChange = (event) => {
@@ -9,12 +10,33 @@ export default function ReportPage(props) {
   // const handlePrintView = (event) => {
   //   return;
   // };
-
+  const handlePrint = () => {
+    window.print();
+  };
+  const handleHomeButton = () => {
+    props.handlePageChange();
+  };
   return (
     <div>
       {/* Palautekenttä? */}
       {/* Stipend */}
       <Stipend classPosition={""} />
+      <Button
+        bgrUrl={
+          "https://cdn0.iconfinder.com/data/icons/print-7/24/Print_Button_printer-512.png"
+        }
+        classPosition={"print-button"}
+        classStyle={"print-button-style"}
+        onClick={handlePrint}
+      />
+      <Button
+        bgrUrl={
+          "https://cdn0.iconfinder.com/data/icons/print-7/24/Print_Button_printer-512.png"
+        }
+        classPosition={"home-button"}
+        classStyle={"print-button-style"}
+        onClick={handleHomeButton}
+      />
     </div>
   );
 }
